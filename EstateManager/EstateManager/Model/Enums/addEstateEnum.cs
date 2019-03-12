25adc53c21a4f7f580d649c9365e7c0d8b153930
p1709400 <quentin.laurent@etu-univ.lyon1.fr>
2019-03-12 12:28:52 +0100

    added form window

---
+++ EstateManager/EstateManager/Model/Enums/addEstateEnum.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstateManager.Model.Enums
+{
+    public enum addEstateEnum
+    {
+        Surface,
+        Type,
+        RoomsCount,
+        BuildDate,
+        FloorCount,
+        FloorNumber,
+        Address,
+        Zip,
+        City,
+        Latitude,
+        Longitude,
+        EnergyEfficiency,
+        MainPhotoId,
+        OwnerId
+    }
+}
--- EstateManager/EstateManager/Model/Estate.cs
+++ EstateManager/EstateManager/Model/Estate.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using EstateManager.Tools;
 using EstateManager.Model.Enums;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 
 namespace EstateManager.Model
 {
@@ -19,94 +20,129 @@ namespace EstateManager.Model
             get { return (int)GetField(); }
             set { SetField(value); }
         }
+
+        [Description("Choose the main Photo (number) :")]
         public int? MainPhotoId
         {
             get { return (int)GetField(); }
             set { SetField(value); }
         }
+
+        [Description("")]
         [ForeignKey(nameof(MainPhotoId))]
         public Photo MainPhoto
         {
             get { return (Photo)GetField(); }
             set { SetField(value); }
         }
+
+        [Description("Owner :")]
         public int OwnerId
         {
             get { return (int)GetField(); }
             set { SetField(value); }
         }
+        
         [ForeignKey(nameof(OwnerId))]
         public Person Owner
         {
             get { return (Person)GetField(); }
             set { SetField(value); }
         }
+
+        [Description("Surface :")]
         public float Surface
         {
             get { return (float)GetField(); }
             set { SetField(value); }
         }
+
+        [Description("type of Estate :")]
         public EstateType Type
         {
             get { return (EstateType)GetField(); }
             set { SetField(value); }
         }
+
+        [Description("Number of rooms :")]
         public int RoomsCount
         {
             get { return (int)GetField(); }
             set { SetField(value); }
         }
+
+        [Description("Build date :")]
         public DateTime ? BuildDate
         {
             get { return (DateTime)GetField(); }
             set { SetField(value); }
         }
+
+        [Description("number of floors")]
         public int FloorCount
         {
             get { return (int)GetField(); }
             set { SetField(value); }
         }
+
+        [Description("floor number of your flat")]
         public int FloorNumber
         {
             get { return (int)GetField(); }
             set { SetField(value); }
         }
+
+        [Description("Address")]
         public string Address
         {
             get { return (string)GetField(); }
             set { SetField(value); }
         }
+
+        [Description("Zip code")]
         public string Zip
         {
             get { return (string)GetField(); }
             set { SetField(value); }
         }
+
+        [Description("City")]
         public string City
         {
             get { return (string)GetField(); }
             set { SetField(value); }
         }
+
+        [Description("Latitude")]
         public double ? Latitude
         {
             get { return (double)GetField(); }
             set { SetField(value); }
         }
+
+        [Description("Longitude")]
         public double ? Longitude
         {
             get { return (double)GetField(); }
             set { SetField(value); }
         }
+
+        [Description("Energy efficiency")]
         public int EnergyEfficiency
         {
             get { return (int)GetField(); }
             set { SetField(value); }
         }
+
+        [Description("Add pictures")]
         [InverseProperty(nameof(Photo.Estate))]
         public ObservableCollection<Photo> Photos
         {
             get { return (ObservableCollection<Photo>)GetField(); }
             set { SetField(value); }
         }
+
+        [Description("type of Contract")]
         [InverseProperty(nameof(Contract.Estate))]
         public ObservableCollection<Contract> Contracts
         {
